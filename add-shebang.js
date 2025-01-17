3b3190faba3c9866b7df27120b3afdb4dfe0995c
huangjiaqi <lilsnake239299@gmail.com>
2025-01-17 09:56:40 +0800

    delete use strict

--- add-shebang.js
+++ add-shebang.js
@@ -1,25 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const filePath = path.join(__dirname, 'bin', 'index.js');
-const shebang = '#!/usr/bin/env node\n';
+const filePath = path.join(__dirname, "bin", "index.js");
+const shebang = "#!/usr/bin/env node\n";
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.readFile(filePath, "utf8", (err, data) => {
   if (err) {
-    console.error('Error reading file:', err);
+    console.error("Error reading file:", err);
     return;
   }
 
   if (!data.startsWith(shebang)) {
-    const updatedData = shebang + data;
-    fs.writeFile(filePath, updatedData, 'utf8', (err) => {
+    const updatedData = shebang + data.replace('"use strict";', "");
+    fs.writeFile(filePath, updatedData, "utf8", (err) => {
       if (err) {
-        console.error('Error writing file:', err);
+        console.error("Error writing file:", err);
       } else {
-        console.log('Shebang added successfully.');
+        console.log("Shebang added successfully.");
       }
     });
   } else {
-    console.log('Shebang already present.');
+    console.log("Shebang already present.");
   }
-}); 
+});
